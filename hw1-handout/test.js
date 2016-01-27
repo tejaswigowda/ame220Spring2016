b71eb318d91ee8904789410fb343680a02aecb69
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-01-27 12:28:59 -0700

    improved hw1 handout

--- hw1-handout/test.js
+++ hw1-handout/test.js
@@ -1,48 +1,51 @@
-var boxStatus = [
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left",
-  "left"
+var easingsList = [
+"swing",
+"easeInQuad",
+"easeOutQuad",
+"easeInOutQuad",
+"easeInCubic",
+"easeOutCubic",
+"easeInOutCubic",
+"easeInQuart",
+"easeOutQuart",
+"easeInOutQuart",
+"easeInQuint",
+"easeOutQuint",
+"easeInOutQuint",
+"easeInSine ",
+"easeOutSine",
+"easeInOutSine",
+"easeInExpo",
+"easeOutExpo",
+"easeInOutExpo",
+"easeInCirc",
+"easeOutCirc",
+"easeInOutCirc",
+"easeInElasic",
+"easeOutElastic",
+"easeInOutElatic",
+"easeInBack",
+"easeOutBack",
+"easeInOutBack",
+"easeInBounce",
+"easeOutBounce"
 ];
 
+var moveRight = function(){
+}
 
-var buttonClicked = function(n, easing)
+var moveLeft = function(){
+}
+
+var moveBoxLeft= function(n, easing, endcolor, duration)
 {
   var id = "#button" + n.toString();
   var pageWidth = $("body").width();
   var boxWidth = 150;
   if(boxStatus[n] == "left"){
-    $(id).animate({"margin-left" : pageWidth - boxWidth + "px", "color": "red"}, 1000, easing); 
-    boxStatus[n] = "right";
+    $(id).animate({"margin-left" : pageWidth - boxWidth + "px", "color": endcolor}, duration, easing); 
   }
   else{
-    $(id).animate({"margin-left" : "0px", "color" : "black"}, 1000, easing); 
-    boxStatus[n] = "left";
+    $(id).animate({"margin-left" : "0px", "color" : endColor}, duraion, easing); 
   }
 }
