a27cbbca2db468e57b014e55e936ccc1f060ff42
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-02-29 18:10:53 -0700

    in class

--- basicJS/first.js
+++ basicJS/first.js
@@ -1,2 +1,19 @@
 var x = 1;
 console.log (x  + " is a " + typeof x);
+
+
+var calcSigmaN = function(n){
+  var r = ( (n + 1) * n ) / 2;
+  return r;
+}
+  
+var add = function(a, b){
+    var r = a + b;
+    return r;
+}
+
+var n = 100;
+console.log(( (n + 1) * n ) / 2);
+console.log(calcSigmaN(100));
+console.log(calcSigmaN(10));
+console.log(calcSigmaN(999));
