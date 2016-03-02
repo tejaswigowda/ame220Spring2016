c4ba55a76fb48ae298af6b33f96bef0821b2c5f8
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-03-02 14:25:55 -0700

    in class

--- basicJS/first.js
+++ basicJS/first.js
@@ -6,10 +6,51 @@ var calcSigmaN = function(n){
   var r = ( (n + 1) * n ) / 2;
   return r;
 }
-  
+
+var calcSigmaNBF = function(n)
+{
+  var result = 0;
+  var i = 1;
+  while(n >= i){
+    result = result + i;
+    i = i + 1;
+  }
+  return result;
+}
+ 
 var add = function(a, b){
     var r = a + b;
     return r;
+}
+
+var sub = function(a, b){
+    var r = a - b;
+    return r;
+}
+
+var greaterThan = function(a, b){
+  if(a >= b){
+    return a;
+  }
+  return b;
+}
+
+
+var greatestof3 = function(a, b, c){
+  if(a >= b && a >= c){
+    return a;
+  }
+  /*
+  else{
+    if(b >= a && b >= c){
+      return b;
+    }
+  }
+  */
+  else if(b >= a && b >= c){
+      return b;
+  }
+  return c;
 }
 
 var n = 100;
